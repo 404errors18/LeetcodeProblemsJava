918e502faff0d26f039df778505f764b6e7dacb6
404errors18 <1770788996@qq.com>
2025-09-02 00:01:03 +0800

    AC

--- src/main/java/leetcode/easy/P13_RomanToInteger.java
+++ src/main/java/leetcode/easy/P13_RomanToInteger.java
@@ -1,7 +1,28 @@
 package leetcode.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class P13_RomanToInteger {
     public int romanToInt(String s) {
-        // todo
+        Map<Character, Integer> map = Map.of(
+                'I', 1,
+                'V', 5,
+                'X', 10,
+                'L', 50,
+                'C', 100,
+                'D', 500,
+                'M', 1000);
+        int result = map.get(s.charAt(s.length() - 1));
+        for (int i = 0; i < s.length() - 1; i++) {
+            int a = map.get(s.charAt(i));
+            int b = map.get(s.charAt(i + 1));
+            if (a < b) {
+                result -= a;
+            } else {
+                result += a;
+            }
+        }
+        return result;
     }
 }
--- src/test/java/leetcode/easy/P13_RomanToIntegerTest.java
+++ src/test/java/leetcode/easy/P13_RomanToIntegerTest.java
@@ -14,6 +14,10 @@ public class P13_RomanToIntegerTest {
             "III, 3",
             "IV, 4",
             "IX, 9",
+            "XL, 40",
+            "XC, 90",
+            "CD, 400",
+            "CM, 900",
             "LVIII, 58",
             "MCMXCIV, 1994",
     })
